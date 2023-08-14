d29bad5dfac8029186f2dd33838fb7ca8c088cbd
RANJITH <106427495+ranjithranjit@users.noreply.github.com>
2023-08-15 03:15:58 +0530

    Add files via upload

---
+++ public/public/js/index.js
@@ -0,0 +1,24 @@
+
+
+
+const menu = document.querySelector('#menu-icon');
+const navlist = document.querySelector('.nav-list');
+
+
+menu.onclick = ()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
+
+const sr = ScrollReveal({
+    distance: '65px', 
+    duration: 2600,
+    delay: 450,  
+    reset: true
+
+});
+
+sr.reveal(".hero-text", {delay: 200, origin:'top'});
+sr.reveal(".icons", {delay: 500, origin:'left'});
+sr.reveal(".scroll-down", {delay: 200, origin:'right'});
+sr.reveal(".hero-img", {delay: 450, origin:'top'});
